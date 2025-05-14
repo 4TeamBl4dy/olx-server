8f8d6f5e70b9e401533d811b187c653496e2a0fc
Rybka <nikitarybalkin1@gmail.com>
2025-05-14 19:54:59 +0500

    ?

--- middleware/role.js
+++ middleware/role.js
@@ -1,11 +1,12 @@
-function authorizeRole(requiredRole) {
+function authorizeRole(...allowedRoles) {
     return (req, res, next) => {
-        if (req.user && req.user.role === requiredRole) {
+        if (req.user && allowedRoles.includes(req.user.role)) {
             next();
         } else {
             return res.status(403).json({ message: 'Недостаточно прав' });
         }
     };
 }
+
 
 module.exports = { authorizeRole };
